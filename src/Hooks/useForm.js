25a2b07d6f2929d7f3e970b9563656f3f451c8e3
klevessonroseno <klevesson.developer@gmail.com>
2022-11-18 19:48:48 -0300

    Create a new user

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,7 +4,11 @@ const types = {
   email: {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido.'
-  }
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: 'A senha deve conter 1 dígito, 1 letra minúscula, 1 letra maiúscula e pelo menos 8 caracteres.',
+  },
 }
 
 const useForm = (type) => {
