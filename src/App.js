e6ea6cb27719bd2a8e80a9006f6d136fded47bf1
klevessonroseno <klevesson.developer@gmail.com>
2022-11-23 14:20:17 -0300

    Creating NotFound Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Login from './Components/Login/Login';
 import User from './Components/User/User';
 import Photo from './Components/Photo/Photo';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -28,6 +30,8 @@ const App = () => {
               </ProtectedRoute>
             } />
             <Route path='foto/:id' element={<Photo />} />
+            <Route path='perfil/:user' element={<UserProfile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
