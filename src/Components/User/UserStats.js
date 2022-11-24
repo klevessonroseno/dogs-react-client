7e9b5c72bafb4ca65d9364b2c4dbb98f5c5f91bb
klevessonroseno <klevesson.developer@gmail.com>
2022-11-24 11:10:34 -0300

    Generating graphs with external lib Victory

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <>
-      <Head
-        title={'Estatísticas'}
-        description={'Página para visualização das estatísticas do usuário do site Dogs.'}
-      />
-      <div>UserStats</div>
-    </>
-  )
+  const {
+    data,
+    error,
+    loading,
+    request
+  } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem('token');
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data)
+    return (
+      <>
+        <Head
+          title={'Estatísticas'}
+          description={'Página para visualização das estatísticas do usuário do site Dogs.'}
+        />
+        <div><UserStatsGraphs data={data} /></div>
+      </>
+    )
+  else return null;
 }
 
 export default UserStats;
